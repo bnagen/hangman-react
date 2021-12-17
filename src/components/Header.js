f87969c70b9ffb5a411221a7a466f64b9f2a8bbe
br <dxrock1@gmail.com>
2021-12-17 08:01:48 -0800

    working on ui changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,17 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-         <h1>Hangman</h1>
-         <p>Find the word! - Enter a letter</p>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
+        <nav>
+        <div class="nav-wrapper black">
+            <div class="container">
+                <a href="#" class="brand-logo center">Hangman</a>
+            </div>
+        </div>
+    </nav>
+         
+         <p class="text">Find the word! - Enter a letter</p>
          {/* <p className="categories-style">Categories!</p>
          <button type="button" id="athletes" className="button-style">Athletes!</button> 
          <button type="button" id="tv" className="button-style">Television!</button> 
